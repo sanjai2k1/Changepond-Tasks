54823741cbc4b6f8d9b9ec07f1b64ab0892fff8f
sanjai2k1 <sanjai2k1j@gmail.com>
2024-07-25 14:19:03 +0530

    python tasks

--- Python Tasks/specialchars.py
+++ Python Tasks/specialchars.py
@@ -1,37 +1,37 @@
 def main():
     special_characters = [
-'"',  
-"'",  
-'\\', 
-'{',  
-'}',  
-'[',  
-']',  
-'(',  
-')',  
-'<',  
-'>',  
-'&',  
-'*',  
-'%',  
-'$', 
-'!',  
-'@',  
-'#',  
-'^',  
-'~', 
-'`',  
-'|',  
-':',  
-';',  
-',',  
-'.',  
-'?',  
-'/',  
-'-',  
-'_',  
-'=',  
-'+'
+'"',  # Double quotation mark
+"'",  # Single quotation mark
+'\\', # Backslash
+'{',  # Left curly brace
+'}',  # Right curly brace
+'[',  # Left square bracket
+']',  # Right square bracket
+'(',  # Left parenthesis
+')',  # Right parenthesis
+'<',  # Less than / Left angle bracket
+'>',  # Greater than / Right angle bracket
+'&',  # Ampersand
+'*',  # Asterisk
+'%',  # Percent sign
+'$',  # Dollar sign
+'!',  # Exclamation mark
+'@',  # At symbol
+'#',  # Hash / Pound sign
+'^',  # Caret
+'~',  # Tilde
+'`',  # Backtick
+'|',  # Vertical bar / Pipe
+':',  # Colon
+';',  # Semicolon
+',',  # Comma
+'.',  # Period / Dot
+'?',  # Question mark
+'/',  # Forward slash
+'-',  # Hyphen / Dash
+'_',  # Underscore
+'=',  # Equals sign
+'+',  # Plus sign
 ]
     string = input("enter string : ")
     for i in string:
