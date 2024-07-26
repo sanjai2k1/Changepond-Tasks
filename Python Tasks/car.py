d5b13b0b9a928cda7de2c08f571ef06850de3c44
sanjai2k1 <sanjai2k1j@gmail.com>
2024-07-26 17:42:40 +0530

    python

--- Python Tasks/car.py
+++ Python Tasks/car.py
@@ -4,7 +4,16 @@ def make_car(manufacturer,model,**kwargs):
     for key,value in kwargs.items():
         print(key,value)
 def main():
-
-    make_car('Ford' , 'outback' ,color = 'Blue' , tow_package = True)
+    cars={} 
+    manufacturer = input("enter manufacturer :")
+    model = input("enter model : ")
+    key='car'
+    value=[ input("enter color: "),
+           input('enter tow-package : ')
+           ]
+    cars[key]=value
+    lst=[manufacturer,model]
+    make_car(*lst,**cars)
+ 
 if __name__=="__main__":
     main()
--- Python Tasks/sandwich.py
+++ Python Tasks/sandwich.py
@@ -1,10 +1,23 @@
 def printlist(*args):
-    print("your added items on sandwich")
-    for i in args:
+    print(args)
+    print("your choosed : ",args[0])
+    print("your topings are : ")
+    for i in args[1]:
         print(i)
 def main():
-    size=int(input("number of items you want to add: "))
-    lst=[input("enter items: ") for i in range(size)]
-    printlist(*lst)
+    size=int(input("number of sandwiches : "))
+    lst=[(input("enter sandwich : "),[]) for i in range(size)]
+    print("sandwiches : ",list(map(lambda x: x[0],lst)))
+    select= input("select sandwich : ")
+    ind =-1
+    for i in range(len(lst)):
+        if lst[i][0]==select:
+            ind=i
+            break
+    size=int(input("enter number of topings : "))
+    topings=[input("enter topings : ") for i in range(size)]
+    lst[ind][1].extend(topings)
+    choosed=lst[ind]
+    printlist(*choosed)
 if __name__=="__main__":
     main()
