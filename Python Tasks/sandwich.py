a56b579a3e45238fad88750f3149611606191cd2
sanjai2k1 <sanjai2k1j@gmail.com>
2024-07-26 16:56:48 +0530

    python

--- Python Tasks/sandwich.py
+++ Python Tasks/sandwich.py
@@ -1,9 +1,10 @@
 def printlist(*args):
+    print("your added items on sandwich")
     for i in args:
         print(i)
 def main():
-    size=int(input("number : "))
-    lst=[input("enter : ") for i in range(size)]
+    size=int(input("number of items you want to add: "))
+    lst=[input("enter items: ") for i in range(size)]
     printlist(*lst)
 if __name__=="__main__":
     main()
